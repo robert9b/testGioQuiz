3c7f02807c2f0d672b93494acf911e970875282d
robert.bauer <robert.bauer@CHS60730.intranet.ad.camassd.org>
2014-05-29 13:21:06 -0700

    error fixis

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -6,9 +6,8 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class QuizActivity extends Activity { 
+public class QuizActivity extends Activity {
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
