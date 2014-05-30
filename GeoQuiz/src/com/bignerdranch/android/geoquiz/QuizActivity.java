976094155e5569ae2b715a4a824801fbd9d1d42a
robert.bauer <robert.bauer@CHS60730.intranet.ad.camassd.org>
2014-05-30 13:13:54 -0700

    page 53

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,12 +2,14 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class QuizActivity extends Activity {
+	private static final String TAG = "QuizActivity";
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
@@ -40,6 +42,7 @@ public class QuizActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
 
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -60,7 +63,7 @@ public class QuizActivity extends Activity {
 
 			}
 		});
-		mNextButton = (Button) findViewById(R.id.question_text_view);
+		mNextButton = (Button) findViewById(R.id.next_button);
 		mNextButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -77,6 +80,31 @@ public class QuizActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.quiz, menu);
 		return true;
+	}
+	@Override
+	public void onStart(){
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+	@Override
+	public void onPause(){
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+	@Override
+	public void onResume(){
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+	@Override
+	public void onStop(){
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
 	}
 
 }
